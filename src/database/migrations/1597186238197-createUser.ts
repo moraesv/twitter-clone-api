3f62abe8529ad42a44c003c5f656fda489588bba
Vinicius de Moraes <vimoraescarvalho@gmail.com>
2020-10-22 20:06:02 -0300

    Fix migrations and add db scripts

---
+++ src/database/migrations/1597186238197-createUser.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class createUser1597185323053 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user',
+        columns: [
+          { name: 'id', type: 'int', isPrimary: true, isGenerated: true },
+          { name: 'name', type: 'varchar' },
+          { name: 'username', type: 'varchar' },
+          { name: 'email', type: 'varchar' },
+          { name: 'passwordHash', type: 'varchar' },
+          { name: 'createdAt', type: 'timestamp', default: 'now()' },
+          { name: 'updatedAt', type: 'timestamp', default: 'now()' },
+          { name: 'deletedAt', type: 'timestamp', isNullable: true },
+        ],
+      }),
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('user')
+  }
+}
